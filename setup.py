0642d69c4bc3f90531317ca5a87d0311262e933a
sepca0 <s3pca0@gmail.com>
2026-01-02 15:11:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 import datetime
 
-Path("demo.txt").write_text(
+(Path.home() / "demo.txt").write_text(
     f"setup.py executed at {datetime.datetime.now().isoformat()}\n"
 )
 
